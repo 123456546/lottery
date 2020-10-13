904a8af434440648bcbbc171fa84b467c110f539
omaben <oumayma.bendada@gmail.com>
2020-10-13 17:58:02 +0300

    header&footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <van-tabbar v-model="active" active-color="#3889ff" inactive-color="#999999">
+        <van-tabbar-item icon="wap-home">游戏大厅</van-tabbar-item>
+        <van-tabbar-item icon="gift-card">优惠活动</van-tabbar-item>
+        <van-tabbar-item icon="label">开奖结果</van-tabbar-item>
+        <van-tabbar-item icon="manager">个人中心</van-tabbar-item>
+    </van-tabbar>
+</template>
+<script>
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      active: 0,
+    }
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <van-nav-bar :title="msg" left-text left-arrow class="header_blue">
+    <template #left>
+      <van-icon name="more" dot />
+    </template>
+    <template #right>
+      <van-icon name="setting-o" />
+    </template>
+  </van-nav-bar>
+</template>
+<script>
+
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+  .header_blue{
+      background-image: linear-gradient(to right, #3570f7 , #2797ff);
+
+  }
+  .header_blue .van-icon {
+    color: #fff!important;
+    font-size: 16pt;
+  }
+
+  .header_blue .van-nav-bar__title {
+      color: #fff;
+      font-size: 17pt;
+  }
+</style>
