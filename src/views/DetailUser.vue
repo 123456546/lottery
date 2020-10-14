28aacbf9c4b5881aed0efd969b2b25279d99907b
omaben <oumayma.bendada@gmail.com>
2020-10-14 16:02:51 +0300

    edit user profil

--- src/views/DetailUser.vue
+++ src/views/DetailUser.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <Header msg="个人资料" back="true"/>
-    <van-cell center  title="我的头像" class="top_user">
+    <van-cell center  title="我的头像" class="top_user info">
     </van-cell>
-    <van-cell center  title icon class="top_user">
+    <van-cell center  title icon class="top_user" @click="show = true">
       <template #right-icon>
       </template>
        <template #icon>
         <img alt="user_avatar" src="../assets/user/icone_top_edit.png">
       </template>
     </van-cell>
-    <van-cell center  title="用户名" label="test001" class="top_user">
+    <van-cell center  title="用户名" label="test001" class="top_user info">
     </van-cell>
-    <van-cell-group class="link_user">
+    <van-cell-group class="link_user info">
         <van-cell center title="真实姓名" label="真实姓名需与银行卡持卡人姓名一致，否则无法提款" >
             <template #right-icon>
             <van-icon name="arrow" class="arrow" />
@@ -45,18 +45,56 @@
         </van-cell>
       
     </van-cell-group>
+    <van-cell center  title class="footer_condition">
+        <template #title>
+            <p>为了您的心思安全，信息在确认后将无法修改 如需帮助，请 <span>联系客服</span></p>
+        </template>
+    </van-cell>
+    <van-action-sheet v-model="show" @cancel="onCancel"  cancel-text="取消">
+        <div class="content">
+            <van-grid :border="false" :column-num="4">
+                <van-grid-item v-for="image in images" :key="image">
+                    <img alt=""  :src="image.name" class="profil_user"/>
+                </van-grid-item>
+            </van-grid>
+            <van-cell-group>
+                <van-cell title="从相册选取"  />
+                <van-cell title="拍照"  />
+            </van-cell-group>
+        </div>
+    </van-action-sheet>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
-
+import { Toast } from 'vant';
 export default {
   name: 'DetailUser',
+   data() {
+    return {
+      show: false,
+      images: [
+        { name: require('@/assets/user/person1.png') },
+        { name: require('@/assets/user/person2.png') },
+        { name: require('@/assets/user/person3.png') },
+        { name: require('@/assets/user/person4.png') },
+        { name: require('@/assets/user/person5.png') },
+        { name: require('@/assets/user/person6.png') },
+        { name: require('@/assets/user/person7.png') },
+        { name: require('@/assets/user/person8.png') },
+      ],
+    };
+  },
   components: {
     Header
-  }
+  },
+  methods: {
+    onCancel() {
+      Toast('cancel');
+    },
+  },
 }
 </script>
 <style >
@@ -200,5 +238,30 @@ span.custom_info {
 }
 .link_user .van-icon {
     color: #bdc1ce;
+}
+.top_user.info {
+    padding: 0;
+}
+
+.top_user.info .van-cell__title,.link_user.info .van-cell__title {
+    padding-left: 9px;
+}
+
+
+.link_user.info .van-cell {
+    padding-left: 0;
+}
+
+.link_user.info {
+    padding-top: 0;
+    padding-bottom: 0;
+}
+.footer_condition {background: #f2f4f8;color: #a1a3a6;font-size: 10pt;}
+
+.footer_condition p span {
+    color: #6091ff;
+}
+img.profil_user {
+    width: 60px;
 }
 </style>
